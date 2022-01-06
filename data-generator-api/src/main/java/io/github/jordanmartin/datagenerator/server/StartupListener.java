ab4812493b3e715e6529308a990d67281af731c2
Jordan Martin <jo69270@gmail.com>
2022-01-06 13:22:50 +0100

    Ouverture du navigateur au démarrage du serveur API et définition du port random par défaut

---
+++ data-generator-api/src/main/java/io/github/jordanmartin/datagenerator/server/StartupListener.java
@@ -0,0 +1,71 @@
+package io.github.jordanmartin.datagenerator.server;
+
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.configuration.ProfileManager;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Slf4j
+@ApplicationScoped
+public class StartupListener {
+
+    @ConfigProperty(name = "quarkus.http.port")
+    int httpPort;
+
+    @ConfigProperty(name = "open-browser", defaultValue = "true")
+    boolean openBrowser;
+
+    void onStart(@Observes StartupEvent ev) {
+        if ("prod".equals(ProfileManager.getActiveProfile()) && openBrowser) {
+            openBrowserAsyncWhenPortIsSet();
+        }
+    }
+
+    private void openBrowserAsyncWhenPortIsSet() {
+        new Thread(() -> {
+            if (waitPortIsSet()) {
+                openInBrowser();
+            }
+        }).start();
+    }
+
+    private boolean waitPortIsSet() {
+        int poolTime = 500;
+        int maxTime = 5000;
+        for (int duration = 0; duration < maxTime; duration += poolTime) {
+            this.httpPort = Integer.parseInt(System.getProperty("quarkus.http.port"));
+            if (httpPort != 0) {
+                return true;
+            }
+            try {
+                Thread.sleep(poolTime);
+            } catch (InterruptedException ignored) {
+            }
+        }
+        return false;
+    }
+
+    private void openInBrowser() {
+        String url = "http://localhost:" + httpPort;
+        log.info("Serveur démarré: {}", url);
+        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+        if (desktop == null || !desktop.isSupported(Desktop.Action.BROWSE)) {
+            // There is no default browser
+            return;
+        }
+        try {
+            desktop.browse(new URI(url));
+            log.info("Ouverture automatique du navagateur. Utilisez l'option -Dopen-browser=false pour le désactiver");
+        } catch (IOException | URISyntaxException e) {
+            throw new IllegalStateException("Failed opening the default browser to show the URL (" + url + ").", e);
+        }
+    }
+
+}
