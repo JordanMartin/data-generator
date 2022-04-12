4aa5b9b05520e5073325d9eaf0275fb78e8639fc
Jordan Martin <jo69270@gmail.com>
2022-04-13 01:36:14 +0200

    Filtrage + trie de la documentation des générateurs (#11)

--- data-generator-webui/src/app/components/generator-doc/generator-doc.component.ts
+++ data-generator-webui/src/app/components/generator-doc/generator-doc.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {DataGeneratorApiService} from '../../services/data-generator-api.service';
-import {GeneratorDoc} from '../../services/generator-doc';
-import {GeneratorCtor} from "../../services/generator-ctor";
-import {GeneratorArg} from "../../services/generator-arg";
+import { Component, OnInit } from '@angular/core';
+import { DataGeneratorApiService } from '../../services/data-generator-api.service';
+import { GeneratorDoc } from '../../services/generator-doc';
+import { GeneratorCtor } from '../../services/generator-ctor';
+import { GeneratorArg } from '../../services/generator-arg';
+import { SimplifyJavaTypePipe } from './simplify-java-type.pipe';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-generator-doc',
@@ -11,14 +13,40 @@ import {GeneratorArg} from "../../services/generator-arg";
 })
 export class GeneratorDocComponent implements OnInit {
 
-  generators!: GeneratorDoc[];
+  generators: GeneratorDoc[] = [];
+  generatorsFiltered: GeneratorDoc[] = [];
+  private _search: string = '';
+  private _sort: string = 'name';
 
   constructor(private api: DataGeneratorApiService) {
   }
 
   ngOnInit(): void {
+    const simplifyJavaTypePipe = new SimplifyJavaTypePipe();
     this.api.getAvailableGenerator()
-      .subscribe(generators => this.generators = generators);
+      .subscribe(generators => {
+        generators.forEach(g => g.type = simplifyJavaTypePipe.transform(g.type));
+        this.generators = generators;
+        this.search = '';
+      });
+  }
+
+  set search(value: string) {
+    this._search = value;
+    this.filterGenerators();
+  }
+
+  get search() {
+    return this._search;
+  }
+
+  get sort(): string {
+    return this._sort;
+  }
+
+  set sort(value: string) {
+    this._sort = value;
+    this.filterGenerators();
   }
 
   getCtorUsage(generator: GeneratorDoc, ctor: GeneratorCtor) {
@@ -34,5 +62,33 @@ export class GeneratorDocComponent implements OnInit {
         .map(ex => '"' + ex + '"');
     }
     return examples.join(', ');
+  }
+
+  get preffiledIssueUrl() {
+    let title = encodeURIComponent('Ajouter un générateur de ' + this._search);
+    let body = encodeURIComponent('Description :  \n\nType retourné : \n\nParamètres : ');
+    const labels = 'enhancement';
+    return `https://github.com/JordanMartin/data-generator/issues/new?title=${title}&labels=${labels}&body=${body}`;
+  }
+
+  private sortGenerator(a: GeneratorDoc, b: GeneratorDoc): number {
+    const compareType = (a.type || '').localeCompare(b.type || '');
+    const compareName = (a.name || '').localeCompare(b.name || '');
+    if (this._sort === 'type') {
+      return compareType == 0 ? compareName : compareType;
+    }
+
+    return compareName == 0 ? compareType : compareName;
+  }
+
+  private filterGenerators() {
+    const search = this._search.trim().toLowerCase();
+    const filtered = this.generators.filter(g => {
+      const nameMatch = g.name.toLowerCase().indexOf(search) !== -1;
+      const descriptionMatch = g.description.toLowerCase().indexOf(search) !== -1;
+      const returnTypeMatch = g.type.toLowerCase().indexOf(search) !== -1;
+      return nameMatch || descriptionMatch || returnTypeMatch;
+    });
+    this.generatorsFiltered = filtered.sort(this.sortGenerator.bind(this));
   }
 }
--- data-generator-webui/src/app/services/data-generator-api.service.ts
+++ data-generator-webui/src/app/services/data-generator-api.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { GeneratorDoc } from './generator-doc';
-import { OutputConfig } from '../components/output-config/output-config';
-import { map } from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {GeneratorDoc} from './generator-doc';
+import {OutputConfig} from '../components/output-config/output-config';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -41,7 +41,7 @@ export class DataGeneratorApiService {
       }
     ).pipe(
       map(response => {
-        return Object.values(response).sort(DataGeneratorApiService.sortGenerator);
+        return Object.values(response);
       })
     );
   }
@@ -57,19 +57,4 @@ export class DataGeneratorApiService {
       })
     );
   }
-
-  private static sortGenerator(a: GeneratorDoc, b: GeneratorDoc): number {
-    const compareGroupe = (a.groupe || '').localeCompare(b.groupe || '');
-    const compareType = (a.type || '').localeCompare(b.type || '');
-    const compareName = (a.name || '').localeCompare(b.name || '');
-    if (compareGroupe != 0) {
-      return compareGroupe;
-    }
-    if (compareType != 0) {
-      return compareType;
-    }
-    return compareName;
-  }
-
-
 }
